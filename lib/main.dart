71ab198a26050237a0d729b0944dd42f3b8d4499
Md. Amanur Rahman <amanur.rahman@egeneration.co>
2019-05-15 01:20:01 +0600

    feedback added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:quran/pages/ayalistbysura.dart';
 import 'package:quran/pages/hadislist.dart';
 import 'package:quran/pages/suralist.dart';
-import 'package:contacts_service/contacts_service.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -21,24 +19,65 @@ class BanglaQuran extends StatefulWidget {
 }
 
 class BanglaQuranState extends State<BanglaQuran> {
+  var formKey = GlobalKey<FormState>();
+  var usrName = TextEditingController();
+  var usrEmail = TextEditingController();
+  var usrComments = TextEditingController();
+  var name, email, comments;
+
   @override
   void initState() {
     super.initState();
   }
 
+  void hanfleSubmit() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      if (name != null && email != null) {
+        DatabaseReference reference = FirebaseDatabase.instance.reference();
+        var data = {"name": name, "email": email, "comments": comments};
+        reference.child('feedback').push().set(data).then((onValue) {
+          print('${data} ' + ' data saved');
+          hanfleReset();
+          showToast();
+        });
+      }
+    }
+  }
+
+  void hanfleReset() {
+    Navigator.of(context).pop();
+    usrName.clear();
+    usrEmail.clear();
+    usrComments.clear();
+  }
+
   showToast() {
     Fluttertoast.showToast(
-        msg: "হাদিস টি কপি সম্পন্ন হয়েছে",
+        msg: "Thank you for your valueable feedback",
         toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.CENTER,
+        gravity: ToastGravity.TOP,
         timeInSecForIos: 1,
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         textColor: Colors.white,
         fontSize: 16.0);
   }
 
-  _showDialog() {
+  String validateEmail(String value) {
+    if (value.length == 0) {
+      return ("Email is required");
+    } else {
+      Pattern pattern =
+          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+      RegExp regex = new RegExp(pattern);
+      if (!regex.hasMatch(value))
+        return 'Enter Valid Email';
+      else
+        return null;
+    }
+  }
 
+  _showDialog() {
     return showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -47,46 +86,81 @@ class BanglaQuranState extends State<BanglaQuran> {
               child: ListBody(
                 children: <Widget>[
                   Text("Feedback"),
-                  TextFormField(
-                    decoration:
-                        InputDecoration(labelText: 'Enter your name'),
-                  ),TextFormField(
-                    decoration:
-                        InputDecoration(labelText: 'Enter your email'),
-                  ),TextFormField(
-                    decoration:
-                        InputDecoration(labelText: 'Enter your commet'),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          controller: usrName,
+                          decoration: InputDecoration(labelText: "Your name"),
+                          validator: (value) {
+                            if (value.length == 0) return ("Name is required");
+                          },
+                          onSaved: (value) {
+                            this.name = value;
+                          },
+                        ),
+                        TextFormField(
+                          controller: usrEmail,
+                          decoration:
+                              InputDecoration(labelText: "Your email address"),
+                          validator: validateEmail,
+                          keyboardType: TextInputType.emailAddress,
+                          onSaved: (value) {
+                            this.email = value;
+                          },
+                        ),
+                        TextField(
+                          controller: usrComments,
+                          decoration:
+                              InputDecoration(labelText: "Your Comments"),
+                          minLines: 1,
+                          maxLines: null,
+                          keyboardType: TextInputType.multiline,
+                          onChanged: (value) {
+                            this.comments = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            FlatButton(
+                              padding: EdgeInsets.all(0),
+                              color: Colors.deepOrange,
+                              textColor: Colors.white,
+                              child: Text("Cancel"),
+                              onPressed: () {
+                                hanfleReset();
+                              },
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            FlatButton(
+                              padding: EdgeInsets.all(0),
+                              color: Colors.teal,
+                              textColor: Colors.white,
+                              child: Text("Submit"),
+                              onPressed: () {
+                                hanfleSubmit();
+
+                                // showToast();
+                              },
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 5,),
-                  FlatButton(
-                    padding: EdgeInsets.all(0),
-                    color: Colors.teal,
-                    textColor: Colors.white,
-                    child: Text("Submit"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      showToast();
-                    },
-                  )
                 ],
               ),
             ),
             actions: <Widget>[],
           );
         });
-  }
-
-  sendToTheServer(){
-    DatabaseReference reference = FirebaseDatabase.instance.reference();
-    var data = {
-      "email": "kanaks2010@gmail.com 2",
-      "phone": "01719839141 2",
-      "comment": "this is good application 2"
-    };
-    print('${data} ' +   ' trying to data saved');
-    reference.child('feedback').push().set(data).then((onValue){
-      print('${data} ' +   ' data saved');
-    });
   }
 
   @override
@@ -99,17 +173,14 @@ class BanglaQuranState extends State<BanglaQuran> {
             backgroundColor: Colors.teal,
             iconTheme: IconThemeData(color: Colors.black),
             actions: <Widget>[
-              // action button
               IconButton(
                 color: Colors.white,
                 icon: Icon(
-                  Icons.chrome_reader_mode,
+                  Icons.feedback,
                   size: 15,
                 ),
                 onPressed: () {
-                  print("Icon presed");
-                  // _showDialog();
-                  sendToTheServer();
+                  _showDialog();
                 },
               ),
             ],
